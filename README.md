5ad21e34da374a1644322f0cf0fe514b0e20a0b2
Lucalux <akliasian85@gmail.com>
2024-04-14 20:47:41 -0700

    first

--- README.md
+++ README.md
@@ -1,2 +1,57 @@
-# cs431-631-airports
-Long description
+# Airline Search Engine
+
+Airline Search Engine is an airline search engine that can help users plan trips via air travel, providing possible routes and information about general flights.
+
+CS 431/631 Project
+
+Contributors: Alexander Ram, Mason Smith, Thomas Braun
+
+University of Nevada, Reno - Computer Science and Engineering
+
+Professor: Dr. Lei Yang
+
+## Popular Features
+
+- Explore available airports within a designated country.
+- Discover the airlines operating within a specified country.
+- Determine a travel route between two cities.
+- Locate a direct route between two cities with minimal layovers.
+- Obtain a list of cities easily accessible from another city by air.
+- Identify airlines offering direct flights.
+- Evaluate routes with the fewest layovers en route to your destination.
+
+## Extra Features
+
+- Check for airline codeshare agreements with other carriers.
+- Calculate the total number of airports within a selected country.
+- Investigate which cities are served by the greatest number of airlines.
+
+## Requirements
+
+Our software is currently designed for the Linux terminal (any distro should work), and requires at least Python 3.7 installed.
+
+We use PostgreSQL as our relational database management system.
+
+As our program is a command-line application, the hardware requirements are extremely minimal, and will not be listed here—hardly a device exists that couldn't run the search engine.
+
+## Usage
+
+Simply start up a terminal window and navigate to the installation folder.
+
+Run the following command to start the program, assuming you have Python 3 installed:
+
+```
+python3 airlines.py
+```
+
+## Advanced
+
+Our search engine can be used in scripts for other programs; run the following command for more details:
+
+```
+python3 airlines.py -h
+```
+
+## License
+
+See LICENSE for details.
---
+++ airlines.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+'''
+
+CS 431/631 Project
+
+Authors:
+    Alexander Ram
+    .
+    .
+
+'''
+
+import getopt
+import os
+import sys
+
+def displayMenu(cols, rows):
+    option = ''
+    print("active window is %d columns, %d rows" % (cols, rows))
+    print("what do you want to do? ")
+    print("1 - something")
+    print("0 - leave")
+    option = input()
+    if option == '0':
+        return False
+    elif option == '1':
+        return True
+    else:
+        print("not a valid option")
+        return True
+
+
+def print_help():
+    print("Usage:  airports [-h]")
+    print("        airports [-d] [OPTION]...")
+    print("Search through the airports and airlines database.")
+    print("Returns results in a tabulated text format.")
+    print("\nCommand-line user interface is the default usage.")
+    print("Line-by-line scripting is possible using the options below.")
+    print("\nOptions:")
+    print("  -h, --help               display this help screen")
+    print("  -d, --debug              start program in debug mode")
+
+def main(argv):
+    path = ''
+    menuActive = True
+    cols, rows = os.get_terminal_size()
+    try:
+        opts, args = getopt.getopt(argv, "hd", ["help", "debug"])
+    except getopt.GetoptError as err:
+        # print help and exit
+        print(err)
+        print_help()
+        sys.exit(2)
+    debugString = "Program started in debug mode."
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            print_help()
+            sys.exit()
+        elif o in ("-d", "--debug"):
+            print(debugString)
+        else:
+            assert False, "unhandled option"
+    try:
+        # debug code; edit this later
+        x = 1
+    except:
+        # debug code; edit this later
+        x = 1
+    while menuActive == True:
+        menuActive = displayMenu(cols, rows)
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
